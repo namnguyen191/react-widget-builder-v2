8ebbcb012f4ae8f396c807b9f4e44dda0747b41c
NAM NGUYEN <hoangnamnguyen191@gmaiil.com>
2022-03-30 23:53:18 -0400

    feat: add placeholder in home for graphql feature

--- libs/features/home/src/lib/FeaturesHome.tsx
+++ libs/features/home/src/lib/FeaturesHome.tsx
@@ -1,10 +1,36 @@
-import { Grid } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from '@mui/material';
 
 export const FeaturesHome: React.FC = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <div>FeaturesHome work!</div>
+        <Typography variant="h3">Building widgets with less pain!</Typography>
+      </Grid>
+      <Grid item xs={3}>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary">
+              Editor
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} variant="h5" component="div">
+              Graphql
+            </Typography>
+            <Typography variant="body2">
+              A simple tools to edit, prettify and convert your graphql query to
+              string format
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Go to Graphql!</Button>
+          </CardActions>
+        </Card>
       </Grid>
     </Grid>
   );
