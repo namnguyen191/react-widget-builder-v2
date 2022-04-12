928a52cc237473860e354f74653b4ea77f9e9ccd
NAM NGUYEN <hoangnamnguyen191@gmaiil.com>
2022-04-12 00:16:00 -0400

    feat: add workbox service worker

--- apps/widget-builder/src/main.tsx
+++ apps/widget-builder/src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
+import * as serviceWorkerRegistration from './service-worker/serviceWorkerRegistration';
 
 const appContainer = document.getElementById('root');
 
@@ -21,3 +22,5 @@ if (appContainer) {
 } else {
   console.error('Cannot find element with id "root"');
 }
+
+serviceWorkerRegistration.register();
