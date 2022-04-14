e7d226922e73bd6acfbcdc8f9126acbe045fc1f8
NAM NGUYEN <hoangnamnguyen191@gmaiil.com>
2022-04-13 20:53:37 -0400

    feat: deploy v1.0.1, using env in manifest file

--- apps/widget-builder/webpack.config.js
+++ apps/widget-builder/webpack.config.js
@@ -2,6 +2,7 @@ const nxReactBaseConfig = require('@nrwl/react/plugins/webpack');
 const { merge } = require('webpack-merge');
 const { InjectManifest } = require('workbox-webpack-plugin');
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = function (webpackConfig, nxConfig) {
   // Fix that Nx uses a different attribute when serving the app
@@ -12,6 +13,7 @@ module.exports = function (webpackConfig, nxConfig) {
 
   // For production we add the service worker
   if (config.mode === 'production') {
+    // generate service-worker.js
     mergeWebpackConfigs.push({
       plugins: [
         new InjectManifest({
@@ -23,6 +25,24 @@ module.exports = function (webpackConfig, nxConfig) {
           // this is the output of the plugin,
           // relative to webpack's output directory
           swDest: 'service-worker.js',
+        }),
+      ],
+    });
+
+    mergeWebpackConfigs.push({
+      plugins: [
+        new CopyPlugin({
+          patterns: [
+            {
+              from: 'src/manifest.json',
+              to: 'manifest.json',
+              transform(content) {
+                return content
+                  .toString()
+                  .replace('%NX_PUBLIC_URL%', process.env.NX_PUBLIC_URL);
+              },
+            },
+          ],
         }),
       ],
     });
