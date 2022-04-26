466555c46fa329e8984da21e0460eed74b06164b
NAM NGUYEN <hoangnamnguyen191@gmail.com>
2022-04-26 13:25:35 -0400

    feat: move router base name to env

--- apps/widget-builder/src/environments/environment.prod.ts
+++ apps/widget-builder/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  reactRouterBaseName: '/react-widget-builder-v2',
 };
--- apps/widget-builder/src/main.tsx
+++ apps/widget-builder/src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
+import { environment } from './environments/environment';
 import * as serviceWorkerRegistration from './service-worker/serviceWorkerRegistration';
 
 const appContainer = document.getElementById('root');
@@ -12,7 +13,7 @@ if (appContainer) {
 
   root.render(
     <StrictMode>
-      <BrowserRouter basename="/react-widget-builder-v2">
+      <BrowserRouter basename={environment.reactRouterBaseName}>
         <CssBaseline>
           <App />
         </CssBaseline>
