83fa754a3b4679b2b9a2a71e6fbcf738f588ad82
Nam Nguyen <hoangnamnguyen191@gmail.com>
2023-08-19 21:54:52 -0400

    fix: config base url

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
+import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
+import App from './App.tsx';
 
 const appContainer = document.getElementById('root');
 
 if (!appContainer) {
   throw Error('Cannot find element with id "root"');
 } else {
+  console.log(`Base URL: `, import.meta.env.BASE_URL);
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <CssBaseline>
           <App />
         </CssBaseline>
